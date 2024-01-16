da4d15ec42d3da28db444eb552eb4c7bf5e66337
rhs0402 <rhs0402@naver.com>
2024-01-16 18:11:52 +0900

    2024-01-16

--- src/com/javaex/ex04/PhoneBookApp.java
+++ src/com/javaex/ex04/PhoneBookApp.java
@@ -1,12 +1,18 @@
 package com.javaex.ex04;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhoneBookApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		//쓰기 쓰면 파일 다 지워짐
 		
@@ -20,9 +26,54 @@ public class PhoneBookApp {
 		Reader fr = new FileReader("C:\\javaStudy\\PhoneDB.txt");
 		BufferedReader br = new BufferedReader(fr);
 		
+		List<Person> pList = new ArrayList<Person>();
+
+		while(true) {
+			String str = br.readLine();
+			Person p = new Person();
+			
+			if(str == null) {
+				break;
+			}
+			String[] info = str.split(",");
+			p.setName(info[0]);
+			p.setHp(info[1]);
+			p.setCompany(info[2]);
+			pList.add(p);
+			
+		}
+		
+		Person my = new Person();
+		my.setName("류하석");
+		my.setHp("010-1234-5678");
+		my.setCompany("041-2222-9876");
+		pList.add(my);
 		
 		
+		Writer fw = new FileWriter("C:\\\\javaStudy\\\\PhoneDB.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
 		
+		for(int i=0; i<pList.size(); i++) {
+			bw.write("이름: " + pList.get(i).getName());
+			bw.newLine();
+			bw.write("핸드폰: " + pList.get(i).getHp());
+			bw.newLine();
+			bw.write("회사: " + pList.get(i).getCompany());
+			bw.newLine();
+		}
+		
+		for(int i=0; i<pList.size(); i++) {
+			System.out.println("이름: " + pList.get(i).getName());
+			System.out.println("핸드폰: " + pList.get(i).getHp());
+			System.out.println("회사: " + pList.get(i).getCompany());
+			System.out.println("");
+		}
+	
+		
+		br.close();
+		bw.close();
 	}
+
+
 
 }
